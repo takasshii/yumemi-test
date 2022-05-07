7e747f845e9b40e37132b48200ea603dd7def6c8
たかっしー <takashiho0913@gmail.com>
2022-05-08 05:07:04 +0900

    [add] viewModelの引数をinjectして代入する方針にした。

--- app/src/test/kotlin/jp/co/yumemi/android/code_check/viewModel/OneViewModelTest.kt
+++ app/src/test/kotlin/jp/co/yumemi/android/code_check/viewModel/OneViewModelTest.kt
@@ -1,26 +1,36 @@
 package jp.co.yumemi.android.code_check.viewModel
 
+import androidx.lifecycle.ViewModelProvider
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import jp.co.yumemi.android.code_check.domain.model.api.IApiRepository
+import jp.co.yumemi.android.code_check.domain.model.getResources.IGetResources
 import org.junit.Assert.*
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import javax.inject.Inject
+import javax.inject.Singleton
 
 @HiltAndroidTest
 class OneViewModelTest {
+
+    private lateinit var viewModel: OneViewModel
 
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
     @Inject
-    lateinit var viewModel: OneViewModel
+    lateinit var getResourcesRepository: IGetResources
+
+    @Inject
+    lateinit var apiRepository: IApiRepository
 
     @Before
     fun init() {
         hiltRule.inject()
+        viewModel = OneViewModel(getResourcesRepository,apiRepository)
     }
 
     @After
